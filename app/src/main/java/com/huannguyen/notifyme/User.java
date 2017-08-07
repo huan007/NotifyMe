fb6ffecca0dc7e222bb0a4cf157b951d896afafe
cyraxnguyen <cyraxnguyen@gmail.com>
2017-08-06 22:50:25 -0700

    Finished User class

--- app/src/main/java/com/huannguyen/notifyme/User.java
+++ app/src/main/java/com/huannguyen/notifyme/User.java
@@ -2,7 +2,50 @@ package com.huannguyen.notifyme;
 
 /**
  * Created by Huan Nguyen on 8/6/2017.
+ * Represent a single user in the app. Contains user's information
  */
 
 class User {
+    String userName;
+    String email;
+    UserType userType;
+
+    public User()
+    {
+        userName = null;
+        email = null;
+        userType = UserType.GUEST;
+    }
+
+    public User(String userName, String email, UserType userType)
+    {
+        this.userName = userName;
+        this.email = email;
+        this.userType = userType;
+    }
+
+    /* Setters and Getters (Generated) */
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public UserType getUserType() {
+        return userType;
+    }
+
+    public void setUserType(UserType userType) {
+        this.userType = userType;
+    }
 }
