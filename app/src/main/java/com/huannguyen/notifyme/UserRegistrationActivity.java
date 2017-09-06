34f9145c5fb02c849e8857bdadab23f3cd3639e7
cyraxnguyen <cyraxnguyen@gmail.com>
2017-09-05 18:07:14 -0700

    Added functionalities for UserRegistrationActivity

--- app/src/main/java/com/huannguyen/notifyme/UserRegistrationActivity.java
+++ app/src/main/java/com/huannguyen/notifyme/UserRegistrationActivity.java
@@ -10,24 +10,43 @@ import com.google.firebase.auth.FirebaseUser;
 
 public class UserRegistrationActivity extends AppCompatActivity {
     private static final String TAG = "UserRegistrationActivity";
+    private static FirebaseUser currUser;
+    private static UserManagerInterface userManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Get currUser and UserManager (Using Firebase for now)
+        currUser = FirebaseAuth.getInstance().getCurrentUser();
+        userManager = new UserManagerFirebase();
+
         //OnCreate, check whether the user is already exist
         //FirebaseUser currUser = FirebaseAuth.getInstance().getCurrentUser();
         //Log.d(TAG, "UID: " + currUser.getUid());
 
+        //Skip Activity when the user is already exist
+        startNextActivity();
     }
 
     public void onClickHost(View view)
     {
-
+        User newUser = new User(currUser.getDisplayName(), currUser.getEmail(), currUser.getUid(),
+                UserType.HOST);
+        userManager.registerUser(newUser);
+        startNextActivity();
     }
 
     public void onClickGuest(View view)
+    {
+        User newUser = new User(currUser.getDisplayName(), currUser.getEmail(), currUser.getUid(),
+                UserType.GUEST);
+        userManager.registerUser(newUser);
+        startNextActivity();
+    }
+
+    public void startNextActivity()
     {
 
     }
