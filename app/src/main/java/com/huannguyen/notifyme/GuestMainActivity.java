ba74d53ee4f9aa3c84dce2a415f186b90c1fdf57
Khoa Nguyen <akhoa.nv@gmail.com>
2017-09-22 21:47:50 -0700

    Add UI to Guess Activity. Add new activity named Get Ticket for user to get ticket. Add changable @strings and @colors

--- app/src/main/java/com/huannguyen/notifyme/GuestMainActivity.java
+++ app/src/main/java/com/huannguyen/notifyme/GuestMainActivity.java
@@ -1,7 +1,9 @@
 package com.huannguyen.notifyme;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class GuestMainActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class GuestMainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guest_main);
+    }
+
+    public void onClickTicket(View view) {
+        startTicketActivity();
+    }
+
+    public void startTicketActivity() {
+        Intent ticketIntent = new Intent(this, GetTicketActivity.class);
+        ticketIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(ticketIntent);
     }
 }
