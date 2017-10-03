c6f3f008c869cda0f9b91264abac8d12f9ca18e4
cyraxnguyen <cyraxnguyen@gmail.com>
2017-10-02 22:01:54 -0700

    Added Host/Guest Reservation Interfaces

---
+++ app/src/main/java/com/huannguyen/notifyme/GuestReservationInterface.java
@@ -0,0 +1,9 @@
+package com.huannguyen.notifyme;
+
+/**
+ * Created by Huan Nguyen on 10/2/2017.
+ */
+
+public interface GuestReservationInterface {
+
+}
---
+++ app/src/main/java/com/huannguyen/notifyme/HostReservationInterface.java
@@ -0,0 +1,8 @@
+package com.huannguyen.notifyme;
+
+/**
+ * Created by Huan Nguyen on 10/2/2017.
+ */
+
+public interface HostReservationInterface {
+}
