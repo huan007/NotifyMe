d68fd54c6c65695747edeeaa8398ec87c3d79951
cyraxnguyen <cyraxnguyen@gmail.com>
2017-10-02 22:45:04 -0700

    Updated HostReservationInterface

--- app/src/main/java/com/huannguyen/notifyme/HostReservationInterface.java
+++ app/src/main/java/com/huannguyen/notifyme/HostReservationInterface.java
@@ -5,4 +5,25 @@ package com.huannguyen.notifyme;
  */
 
 public interface HostReservationInterface {
+
+    /**
+     * Create a blank reservation in the Database
+     * @param reservationID unique ID for the reservation
+     * @return created reservation. Return NULL if failed
+     */
+    public Reservation createReservation(String reservationID);
+
+    /**
+     * Delete an existing reservation
+     * @param reservationID unique ID for the reservation
+     * @return return TRUE if successfully deleted the reservation
+     */
+    public boolean deleteReservation(String reservationID);
+
+    /**
+     * Mark the reservation as Ready so customer will be notified
+     * @param reservationID unique ID for the reservation
+     * @return return TRUE if successfully updated the reservation
+     */
+    public boolean setReservationReady(String reservationID);
 }
