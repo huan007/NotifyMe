ade3fd646cbd0af06eab8141bff0b9c885a407b9
Khoa Nguyen <akhoa.nv@gmail.com>
2017-10-05 01:23:59 -0700

    Added QR code reader. Fixed get ticket layout

--- app/src/main/java/com/huannguyen/notifyme/GetTicketActivity.java
+++ app/src/main/java/com/huannguyen/notifyme/GetTicketActivity.java
@@ -1,8 +1,12 @@
 package com.huannguyen.notifyme;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.places.Place;
@@ -13,7 +17,11 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.zxing.client.android.CaptureActivity;
+
 import static com.huannguyen.notifyme.GuestMainActivity.ticket;
+
+
 
 public class GetTicketActivity extends AppCompatActivity implements OnMapReadyCallback {
     private GoogleMap mMap;
@@ -56,5 +64,24 @@ public class GetTicketActivity extends AppCompatActivity implements OnMapReadyCa
 
     public void onClickFinish(View v){
         finish();
+    }
+
+    public void onClickQR(View v){
+        Intent intent = new Intent(getApplicationContext(),CaptureActivity.class);
+        intent.setAction("com.google.zxing.client.android.SCAN");
+        intent.putExtra("SAVE_HISTORY", false);
+        startActivityForResult(intent, 0);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 0) {
+            if (resultCode == RESULT_OK) {
+                String contents = data.getStringExtra("SCAN_RESULT");
+                Log.d("QRCode", "contents: " + contents);
+            } else if (resultCode == RESULT_CANCELED) {
+                Log.d("QRCode", "RESULT_CANCELED");
+            }
+        }
     }
 }
